819b032c3e8c6868c11f49eea6002bd3f6f4c04f
SCaeR <mail@casper.pw>
2026-05-14 02:48:08 +0300

    feat/deploy: добавление принудительной подгрузки env variant 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,40 @@
-import {defineConfig, loadEnv} from 'vite';
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite';
 // import { createHtmlPlugin } from 'vite-plugin-html'
 // import vueDevTools from 'vite-plugin-vue-devtools'
 
 
 // https://vite.dev/config/
-export default defineConfig(({mode}) => {
-    // Загружаем переменные окружения из .env файлов и системного окружения
-    // Третий параметр '' позволяет загружать ВСЕ переменные, а не только с префиксом VITE_
-    const env = loadEnv(mode, process.cwd(), '')
-
+export default defineConfig({
     // base: '/',
-    return {
-        base: env.GITHUB_ACTIONS ? '/pennywise/' : '/',
-        plugins: [
-            vue(),
-            tailwindcss(),
-            // vueDevTools(),
-            // createHtmlPlugin({})
-        ],
-        server: {
-            watch: {
-                // Использовать опрос вместо системных событий
-                usePolling: true,
-                // Интервал опроса в мс (опционально)
-                interval: 100
-            },
-            host: "::",
-            port: 8080,
-            hmr: {
-                overlay: false,
-            },
+    base: process.env.GITHUB_ACTIONS ? '/pennywise/' : '/',
+    plugins: [
+        vue(),
+        tailwindcss(),
+        // vueDevTools(),
+        // createHtmlPlugin({})
+    ],
+    server: {
+        watch: {
+            // Использовать опрос вместо системных событий
+            usePolling: true,
+            // Интервал опроса в мс (опционально)
+            interval: 100
+        },
+        host: "::",
+        port: 8080,
+        hmr: {
+            overlay: false,
+        },
+    },
+
+    resolve: {
+        alias: {
+            // Актуальный способ задания алиаса @ для папки src
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         },
 
-        resolve: {
-            alias: {
-                // Актуальный способ задания алиаса @ для папки src
-                '@': fileURLToPath(new URL('./src', import.meta.url))
-            },
-
-        }
-    }
+    },
 })
-
