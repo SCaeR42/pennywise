a32dc3206860bd8de99fd6e064de5b72e04ef2df
SCaeR <mail@casper.pw>
2026-05-14 02:45:31 +0300

    feat/deploy: добавление принудительной подгрузки env upd1

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({mode}) => {
 
     // base: '/',
     return {
-        base: process.env.GITHUB_ACTIONS ? '/pennywise/' : '/',
+        base: env.GITHUB_ACTIONS ? '/pennywise/' : '/',
         plugins: [
             vue(),
             tailwindcss(),
